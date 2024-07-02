8d16155982d3bac58a4645f9d2751faaabd7e914
yashsomkuwar <yash@getswipe.in>
2024-07-02 12:29:05 +0530

    Initial Commit

---
+++ SnapSavvy/SnapSavvyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnapSavvyApp.swift
+//  SnapSavvy
+//
+//  Created by Yash Somkuwar on 02/07/24.
+//
+
+import SwiftUI
+
+@main
+struct SnapSavvyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
